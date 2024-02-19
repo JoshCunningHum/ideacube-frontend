c8c7eb60a7c009e592b3378a474973d2d81003d9
jasperket <jasperkeith1.vallecera@gmail.com>
2024-02-19 19:47:41 +0800

    Added navigation for user tasks

--- src/routes/_user/$id/task.tsx
+++ src/routes/_user/$id/task.tsx
@@ -1,15 +1,64 @@
-import * as React from 'react'
-import { createFileRoute } from '@tanstack/react-router'
-import { Container } from '@mantine/core'
+import * as React from "react";
+import { createFileRoute } from "@tanstack/react-router";
+import { Anchor, Container, Group, Stack, ThemeIcon } from "@mantine/core";
 
-export const Route = createFileRoute('/_user/$id/task')({
+export const Route = createFileRoute("/_user/$id/task")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
+  const [selected, setSelected] = React.useState("all");
   return (
-      <Container>
-        <h2>Code Here</h2>
-      </Container>
-  )
+    <Container fluid>
+      <Group gap={"1.5rem"}>
+        <Anchor
+          c="black"
+          underline="never"
+          classNames={{
+            root: `cursor-pointer ${selected === "all" ? "border-b-2 border-black -mb-1.5 pb-1" : ""}`,
+          }}
+          unstyled={true}
+          onClick={() => setSelected("all")}
+        >
+          All
+        </Anchor>
+        <Anchor
+          c="black"
+          underline="never"
+          classNames={{
+            root: `cursor-pointer ${selected === "To-do" ? "border-b-2 border-black -mb-1.5 pb-1" : ""}`,
+          }}
+          unstyled={true}
+          onClick={() => setSelected("To-do")}
+        >
+          To-do
+        </Anchor>
+        <Anchor
+          c="black"
+          underline="never"
+          classNames={{
+            root: `cursor-pointer ${selected === "Late" ? "border-b-2 border-black -mb-1.5 pb-1" : ""}`,
+          }}
+          unstyled={true}
+          onClick={() => setSelected("Late")}
+        >
+          Late
+        </Anchor>
+        <Anchor
+          c="black"
+          underline="never"
+          classNames={{
+            root: `cursor-pointer ${selected === "Finished" ? "border-b-2 border-black -mb-1.5 pb-1" : ""}`,
+          }}
+          unstyled={true}
+          onClick={() => setSelected("Finished")}
+        >
+          Finished
+        </Anchor>
+      </Group>
+      <Stack>
+        <Container fluid></Container>
+      </Stack>
+    </Container>
+  );
 }
