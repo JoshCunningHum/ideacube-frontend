557f70600b1b123049fe9dff5a0392e1c3be0091
JoshCunningHum <jkdimpas@gmail.com>
2024-03-01 23:42:04 +0800

    Task Filter uses search params instead of state

--- src/routes/_user/$id/task.tsx
+++ src/routes/_user/$id/task.tsx
@@ -1,118 +1,110 @@
+import { Anchor, Container, Group, Stack } from "@mantine/core";
+import { Link, createFileRoute, useNavigate } from "@tanstack/react-router";
 import * as React from "react";
-import { Link, createFileRoute } from "@tanstack/react-router";
-import {
-  Anchor,
-  Container,
-  Flex,
-  Group,
-  Stack,
-  ThemeIcon,
-} from "@mantine/core";
-import { IconPencil } from "@tabler/icons-react";
 import TaskCard from "../../../components/TaskCard";
+import getEnumKeys from "../../../utils/getEnumKeys";
 
 export const Route = createFileRoute("/_user/$id/task")({
-  component: RouteComponent,
+    component: RouteComponent,
+    validateSearch: (
+        search: Record<string, unknown>
+    ): { filter: TaskStatus | "" } => {
+        return {
+            filter: (search.filter as number) || "",
+        };
+    },
 });
 
+export enum TaskStatus {
+    "To-do" = 1,
+    Late,
+    Finished,
+}
+
 export interface Task {
-  id: number;
-  title: string;
-  deadline: string;
-  status: "To-do" | "Late" | "Finished";
-  type: "Lecture" | "Quiz";
+    id: number;
+    title: string;
+    deadline: string;
+    status: TaskStatus;
+    type: "Lecture" | "Quiz";
 }
 
 const tasks: Task[] = [
-  {
-    id: 1,
-    title: "Lecture 1",
-    deadline: "3:00 PM",
-    status: "To-do",
-    type: "Lecture",
-  },
-  {
-    id: 2,
-    title: "Quiz 1",
-    deadline: "3:00 PM",
-    status: "Late",
-    type: "Quiz",
-  },
-  {
-    id: 3,
-    title: "Lecture 2",
-    deadline: "4:00 PM",
-    status: "Finished",
-    type: "Lecture",
-  },
+    {
+        id: 1,
+        title: "Lecture 1",
+        deadline: "3:00 PM",
+        status: TaskStatus["To-do"],
+        type: "Lecture",
+    },
+    {
+        id: 2,
+        title: "Quiz 1",
+        deadline: "3:00 PM",
+        status: TaskStatus.Late,
+        type: "Quiz",
+    },
+    {
+        id: 3,
+        title: "Lecture 2",
+        deadline: "4:00 PM",
+        status: TaskStatus.Finished,
+        type: "Lecture",
+    },
 ];
 
 function RouteComponent() {
-  const [selected, setSelected] = React.useState("All");
-  const shownTasks = React.useMemo(() => {
-    if (selected === "All") return tasks;
-    return tasks.filter((task) => task.status === selected);
-  }, [selected]);
-  const { id } = Route.useParams();
-  return (
-    <Container fluid classNames={{ root: "p-4 xl:px-8 xl:py-4" }}>
-      <Group gap={"1.5rem"}>
-        <Anchor
-          c="black"
-          underline="never"
-          classNames={{
-            root: `cursor-pointer ${selected === "All" ? "border-b-2 border-black -mb-1.5 pb-1 text-lg" : "hover:border-b-2 hover:border-neutral-400 hover:-mb-1.5 hover:pb-1 text-lg"}`,
-          }}
-          unstyled={true}
-          onClick={() => setSelected("All")}
+    const { filter } = Route.useSearch();
+
+    const navigate = useNavigate({ from: Route.fullPath });
+    const setFilter = (filterType: TaskStatus | "") =>
+        navigate({ search: { filter: filterType }, replace: true });
+
+    const shownTasks = React.useMemo(() => {
+        if (filter === "") return tasks;
+        return tasks.filter((task) => task.status === filter);
+    }, [filter]);
+
+    const { id } = Route.useParams();
+
+    return (
+        <Container
+            fluid
+            classNames={{ root: "p-4 xl:px-8 xl:py-4" }}
         >
-          All
-        </Anchor>
-        <Anchor
-          c="black"
-          underline="never"
-          classNames={{
-            root: `cursor-pointer ${selected === "To-do" ? "border-b-2 border-black -mb-1.5 pb-1 text-lg" : "hover:border-b-2 hover:border-neutral-400 hover:-mb-1.5 hover:pb-1 text-lg"}`,
-          }}
-          unstyled={true}
-          onClick={() => setSelected("To-do")}
-        >
-          To-do
-        </Anchor>
-        <Anchor
-          c="black"
-          underline="never"
-          classNames={{
-            root: `cursor-pointer ${selected === "Late" ? "border-b-2 border-black -mb-1.5 pb-1 text-lg" : "hover:border-b-2 hover:border-neutral-400 hover:-mb-1.5 hover:pb-1 text-lg"}`,
-          }}
-          unstyled={true}
-          onClick={() => setSelected("Late")}
-        >
-          Late
-        </Anchor>
-        <Anchor
-          c="black"
-          underline="never"
-          classNames={{
-            root: `cursor-pointer ${selected === "Finished" ? "border-b-2 border-black -mb-1.5 pb-1 text-lg" : "hover:border-b-2 hover:border-neutral-400 hover:-mb-1.5 hover:pb-1 text-lg"}`,
-          }}
-          unstyled={true}
-          onClick={() => setSelected("Finished")}
-        >
-          Finished
-        </Anchor>
-      </Group>
-      <Stack classNames={{ root: "pt-8" }}>
-        {shownTasks.map((task) => (
-          <Link
-            to="/$id/task/$taskid"
-            params={{ id: id, taskid: task.id.toString() }}
-            key={task.id}
-          >
-            <TaskCard task={task} />
-          </Link>
-        ))}
-      </Stack>
-    </Container>
-  );
+            <Group gap={"1.5rem"}>
+                {["All", ...getEnumKeys(TaskStatus)].map((v, i) => (
+                    <Anchor
+                        key={v}
+                        c="black"
+                        underline="never"
+                        classNames={{
+                            root: `cursor-pointer 
+                              ${
+                                  (!i && !filter) ||
+                                  v === TaskStatus[filter as TaskStatus]
+                                      ? "border-b-2 border-black -mb-1.5 pb-1 text-lg"
+                                      : "hover:border-b-2 hover:border-neutral-400 hover:-mb-1.5 hover:pb-1 text-lg"
+                              }`,
+                        }}
+                        unstyled={true}
+                        onClick={() => setFilter(i === 0 ? "" : i)}
+                    >
+                        {v}
+                    </Anchor>
+                ))}
+            </Group>
+            <Stack classNames={{ root: "pt-8" }}>
+                {shownTasks.map((task) => (
+                    <Link
+                        to="/$id/task/$taskid"
+                        params={{ id: id, taskid: task.id.toString() }}
+                        key={task.id}
+                    >
+                        <TaskCard task={task} />
+                    </Link>
+                ))}
+            </Stack>
+        </Container>
+    );
 }
