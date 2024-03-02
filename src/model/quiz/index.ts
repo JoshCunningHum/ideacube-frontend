cb4ab0d00031e7f476b9e4902308bb0e47826ba2
JoshCunningHum <jkdimpas@gmail.com>
2024-03-02 18:29:55 +0800

    generate-types

---
+++ src/model/quiz/index.ts
@@ -0,0 +1,10 @@
+export interface QuizSerializer {
+    id?: number;
+    title: string;
+    description: string;
+    in_class?: any;
+    in_class_id: number;
+    opens: string;
+    closes: string;
+}
+
