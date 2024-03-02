97dc46382314353f2ba393cb5cbbf3c5ee8439e2
jasperket <jasperkeith1.vallecera@gmail.com>
2024-03-02 15:57:32 +0800

    Made design for file.tsx

--- src/routes/_user/$id/file.tsx
+++ src/routes/_user/$id/file.tsx
@@ -1,15 +1,33 @@
-import * as React from 'react'
-import { createFileRoute } from '@tanstack/react-router'
-import { Container } from '@mantine/core'
+import * as React from "react";
+import { createFileRoute } from "@tanstack/react-router";
+import { Container, FileInput, Group, Text, Title } from "@mantine/core";
+import { IconPlus } from "@tabler/icons-react";
 
-export const Route = createFileRoute('/_user/$id/file')({
+export const Route = createFileRoute("/_user/$id/file")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
   return (
-      <Container>
-        <h2>Code Here</h2>
+    <Container fluid>
+      <Container fluid p={0}>
+        <Group>
+          <Text>options here</Text>
+        </Group>
       </Container>
-  )
+      <Title
+        order={2}
+        mt={"md"}
+      >{`Documents > Quizzes (basta file path)`}</Title>
+      <FileInput
+        leftSection={<IconPlus size={32} />}
+        placeholder="Upload a file"
+        classNames={{
+          input: "bg-white py-4 px-16 border-gray-400",
+          section: "ml-4",
+        }}
+        mt={"md"}
+      />
+    </Container>
+  );
 }
